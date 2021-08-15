e12c3a644f9c33e76730aa87050b898e7a57d80b
Xentripetal <cyruscmartien@gmail.com>
2021-08-15 15:37:30 -0500

    Rename

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,19 @@
 #[cfg(target_arch = "wasm32")]
 use bevy_webgl2;
 
-use bevy::prelude::{App, ClearColor, Color, WindowDescriptor};
+use bevy::prelude::{App, ClearColor, Color, WindowDescriptor, Msaa};
 use bevy::DefaultPlugins;
-use game_plugin::GamePlugin;
+use verse::GamePlugin;
 
 fn main() {
     let mut app = App::build();
     app
-        // .insert_resource(Msaa { samples: 4 })
+        //.insert_resource(Msaa { samples: 4 })
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
         .insert_resource(WindowDescriptor {
             width: 800.,
             height: 600.,
-            title: "Bevy game".to_string(), // ToDo
+            title: "Project Verse".to_string(), // ToDo
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
